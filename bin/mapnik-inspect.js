dcb751c8bda7d5d68f0a75e1d1810f5bac40b758
Dane Springmeyer <dane@dbsgeo.com>
2011-01-21 19:23:51 -0500

    add more potential ogr supported formats to inspect

--- bin/mapnik-inspect.js
+++ bin/mapnik-inspect.js
@@ -2,9 +2,11 @@
 
 var path = require('path');
 
-var usage = 'usage:\n  inspect.js <datasource> (.shp|.json)';
-usage += '\n  inspect.js <stylesheet> (.xml)';
-usage += '\n  inspect.js <projection> (.prj)';
+var usage = 'usage:';
+usage += '\n  mapnik-inspect.js <datasource> (.shp|.json|.geojson|.kml|.sqlite|.gml|.vrt|.csv)';
+usage += '\n  mapnik-inspect.js <stylesheet> (.xml)';
+usage += '\n  mapnik-inspect.js <projection> (.prj)';
+usage += '\n  mapnik-inspect.js <zipfile> (.zip)';
 
 var obj = process.ARGV[2];
 if (!obj) {
@@ -19,11 +21,22 @@ if (!path.existsSync(obj)) {
 
 var mapnik = require('mapnik');
 
+console.log(obj);
+
 if (/.shp$/.test(obj)) {
     var opened = new mapnik.Datasource({type: 'shape', file: obj});
     console.log(opened.describe());
 }
-else if ((/.json$/.test(obj)) || (/.geojson$/.test(obj))) {
+else if ((/.json$/.test(obj))
+         || (/.geojson$/.test(obj))
+         || (/.kml$/.test(obj))
+         || (/.sqlite3$/.test(obj))
+         || (/.db$/.test(obj))
+         || (/.gml$/.test(obj))
+         || (/.vrt$/.test(obj))
+         || (/.csv$/.test(obj))
+         || (/.dbf$/.test(obj))
+        ) {
     var opened = new mapnik.Datasource({type: 'ogr', file: obj, 'layer_by_index': 0});
     console.log(opened.describe());
 }
@@ -36,7 +49,12 @@ else if (/.prj$/.test(obj)) {
     var srs = require('srs');
     console.log(srs.parse(obj));
 }
+else if (/.zip$/.test(obj)) {
+    var zip = require('zipfile');
+    var zf = new zip.ZipFile(obj);
+    console.log(zf);
+}
 else {
-    console.log('only currently supports shapefiles and geojson datasources or mapnik xml');
+    console.log('Error: unsupported file, unable to inspect');
     process.exit(1);
 }
