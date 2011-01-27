3deba2fccfd694e6170895aee6da725416bc8adc
Dane Springmeyer <dane@dbsgeo.com>
2011-01-27 13:40:31 -0800

    add sample http server from readme

---
+++ examples/simple.js
@@ -0,0 +1,19 @@
+var mapnik = require('mapnik');
+var http = require('http');
+
+var port = 8000;
+
+http.createServer(function (req, res) {
+  var map = new mapnik.Map(256,256);
+  map.load("./examples/stylesheet.xml");
+  map.zoom_all();
+  map.render(map.extent(),"png",function(err,buffer){
+      if (err) {
+        res.writeHead(500, {'Content-Type':'text/plain'});
+        res.end(err.message);
+      } else {
+        res.writeHead(200, {'Content-Type':'image/png'});
+        res.end(buffer);
+      }
+  });
+}).listen(port);
