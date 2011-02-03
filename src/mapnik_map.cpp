35fdfc87a44b7f91af76e91938a3683c6ceb8cb0
Dane Springmeyer <dane@dbsgeo.com>
2011-02-03 14:30:08 -0800

    woah

--- src/mapnik_map.cpp
+++ src/mapnik_map.cpp
@@ -54,7 +54,7 @@ void Map::Initialize(Handle<Object> target) {
     NODE_SET_PROTOTYPE_METHOD(constructor, "save", save);
     NODE_SET_PROTOTYPE_METHOD(constructor, "clear", clear);
     NODE_SET_PROTOTYPE_METHOD(constructor, "from_string", from_string);
-    NODE_SET_PROTOTYPE_METHOD(constructor, "to_string", toString);
+    NODE_SET_PROTOTYPE_METHOD(constructor, "toString", to_string);
     NODE_SET_PROTOTYPE_METHOD(constructor, "resize", resize);
     NODE_SET_PROTOTYPE_METHOD(constructor, "width", width);
     NODE_SET_PROTOTYPE_METHOD(constructor, "height", height);
