856e6374eee37a2dcfd90902420a9a6c2b557862
Dane Springmeyer <dane@dbsgeo.com>
2011-02-02 17:49:12 -0800

    fixup express example for dumb people like me that run node versions that are not ancient

--- examples/simple_express.js
+++ examples/simple_express.js
@@ -3,13 +3,20 @@
 var mapnik = require('mapnik');
 var express = require('express');
 
+var ver = process.versions.node.split('.');
+
+if (parseInt(ver[1]) > 2) {
+    console.log('sorry express is currently only compatible with node <= v0.2.6 (you are running: ' + process.version + ')');
+    process.exit();
+}
+
 var app = express.createServer();
 
 var port = 8000;
 
 app.get('/', function(req, res){
   var map = new mapnik.Map(256,256);
-  //map.load("./examples/stylesheet.xml");
+  map.load("./examples/stylesheet.xml");
   map.zoom_all();
   map.render(map.extent(),"png",function(err,buffer){
       if (err) {
@@ -17,9 +24,7 @@ app.get('/', function(req, res){
         res.contentType('.txt');
         res.send(err.message);
       } else {
-        //res.header('Content-Type','image/png');
-        res.contentType("tile.png");
-        res.send(buffer.toString())
+        res.send(buffer, {'Content-Type': 'image/png'});
       }
   });
 })
