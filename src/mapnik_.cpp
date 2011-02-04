61f204d7bfbe3f85d9d1cfb5aff9fa94126315f6
Dane Springmeyer <dane@dbsgeo.com>
2011-02-03 22:23:40 -0800

    add template cpp/hpp files

---
+++ src/mapnik_.cpp
@@ -0,0 +1,27 @@
+#include "mapnik_•.hpp"
+
+Persistent<FunctionTemplate> •::constructor;
+
+void •::Initialize(Handle<Object> target) {
+
+    HandleScope scope;
+  
+    constructor = Persistent<FunctionTemplate>::New(FunctionTemplate::New(•::New));
+    constructor->InstanceTemplate()->SetInternalFieldCount(1);
+    constructor->SetClassName(String::NewSymbol("•"));
+
+    target->Set(String::NewSymbol("•"),constructor->GetFunction());
+}
+
+•::•(std::string const& name) :
+  ObjectWrap(),
+  this_(new mapnik::•(name)) {}
+
+•::~•()
+{
+}
+
+Handle<Value> •::New(const Arguments& args)
+{
+  HandleScope scope;
+}
---
+++ src/mapnik_.hpp
@@ -0,0 +1,27 @@
+#ifndef __NODE_MAPNIK_•_H__
+#define __NODE_MAPNIK_•_H__
+
+#include <v8.h>
+#include <node.h>
+#include <node_object_wrap.h>
+
+using namespace v8;
+using namespace node;
+
+typedef boost::shared_ptr<mapnik::•> •_ptr;
+
+class •: public node::ObjectWrap {
+  public:
+    static Persistent<FunctionTemplate> constructor;
+    static void Initialize(Handle<Object> target);
+    static Handle<Value> New(const Arguments &args);
+    
+    •(std::string const& name);
+    •(std::string const& name, std::string const& srs);
+
+  private:
+    ~•();
+    •_ptr this_;
+};
+
+#endif
