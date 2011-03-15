9fb5cffad0301f24640d63ae9825025e76239836
Konstantin Käfer <github@kkaefer.com>
2011-03-15 14:39:12 -0400

    expresso test

---
+++ test/render_grid.test.js
@@ -0,0 +1,23 @@
+var mapnik = require('../lib');
+var assert = require('assert');
+var fs = require('fs');
+var util = require('util');
+
+exports['test grid rendering'] = function(beforeExit) {
+    var rendered = false;
+    var reference = fs.readFileSync('./test/support/simple_grid.json', 'utf8');
+
+    var map_grid = new mapnik.Map(256, 256);
+    map_grid.load('./examples/stylesheet.xml');
+    map_grid.zoom_all();
+
+    map_grid._render_grid(0, 4, 'FIPS', function(err, grid) {
+        rendered = true;
+        assert.ok(!err);
+        assert.equal(JSON.stringify(grid), reference);
+    });
+
+    beforeExit(function() {
+        assert.ok(rendered);
+    });
+};
