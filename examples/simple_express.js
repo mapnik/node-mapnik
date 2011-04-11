360d47ee17ae3c1a376f9f13498b977a0c9d2a8f
Simon Tokumine <simon@japancentre.com>
2011-04-11 22:27:27 +0100

    updated node_express example

--- examples/simple_express.js
+++ examples/simple_express.js
@@ -1,27 +1,22 @@
 #!/usr/bin/env node
 
-var mapnik = require('mapnik');
-var express = require('express');
-var path = require('path');
+// This example shows how to use node-mapnik with the 
+// express web development framework (http://expressjs.com/)  
+//
+// expected output: http://goo.gl/cyGwo
 
-var ver = process.versions.node.split('.');
-
-if (parseInt(ver[1]) > 2) {
-    console.log('sorry express is currently only compatible with node <= v0.2.6 (you are running: ' + process.version + ')');
-    process.exit();
-}
-
-var app = express.createServer();
-
-var port = 8000;
+var mapnik = require('mapnik')
+  , express = require('express')
+  , path = require('path')
+  , app = express.createServer()
+  , port = 8000;
 
 app.get('/', function(req, res) {
   var map = new mapnik.Map(256, 256);
   map.load(path.join(__dirname, './stylesheet.xml'));
   map.zoom_all();
   map.render(map.extent(), 'png', function(err,buffer) {
-      if (err) {
-        //res.send('Tile not found', 500);
+      if (err) {       
         res.contentType('.txt');
         res.send(err.message);
       } else {
