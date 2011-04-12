adb4ffcef44590a3f8e103ab3822a76058e1412a
Simon Tokumine <simon@japancentre.com>
2011-04-12 14:58:44 +0100

    added simple tiler example

---
+++ examples/tile/simple/app.js
@@ -0,0 +1,60 @@
+#!/usr/bin/env node
+
+// This example shows how to use node-mapnik with the 
+// connect http server to serve map tiles to protovis
+// client
+//
+// expected output at zoom 0: http://goo.gl/cyGwo
+
+
+var mapnik = require('mapnik')
+  , mercator = require('mapnik/sphericalmercator')
+  , connect = require('connect')
+  , url = require('url')
+  , fs = require('fs')
+  , path = require('path')
+  , port = 3000;
+
+
+var server = connect.createServer(  
+  
+  connect.logger('\033[90m:method\033[0m \033[36m:url\033[0m \033[90m:status :response-timems -> :res[Content-Type]\033[0m')  
+, connect.static(__dirname + '/public/')
+, connect.router(function(app){
+    
+    // TILE REQUEST URL
+    app.get('/:x/:y/:z', function(req, res, next){      
+
+      try {
+        // CALCULATE BBOX FOR RENDER STEP
+        var bbox = mercator.xyz_to_envelope(parseInt(req.params.x),
+                                            parseInt(req.params.y),
+                                            parseInt(req.params.z), false);
+      
+        // CREATE MAP
+        var map = new mapnik.Map(256, 256, mercator.srs);
+        map.load(path.join(__dirname, '../../stylesheet.xml'));
+        map.zoom_all();
+                  
+        // RENDER MAP AS PNG
+        map.render(bbox, 'png', function(err, buffer) {
+          if (err) {
+            throw err;
+          } else {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'image/png');        
+            res.end(buffer);            
+          }
+        });
+      }
+      catch (err) {        
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'text/plain');        
+        res.end(err.message);
+      }
+    });
+  })  
+);
+
+server.listen(port);
+console.log("Server running on port " + port);
