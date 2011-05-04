dd5ba88580d3412108aad48be9a13e61949e3b54
Dane Springmeyer <dane@dbsgeo.com>
2011-05-03 18:50:26 -0700

    pushing version to 0.3.1

--- src/_mapnik.cc
+++ src/_mapnik.cc
@@ -200,7 +200,7 @@ extern "C" {
     MemoryDatasource::Initialize(target);
 
     // node-mapnik version
-    target->Set(String::NewSymbol("version"), String::New("0.2.13"));
+    target->Set(String::NewSymbol("version"), String::New("0.3.1"));
 
     // versions of deps
     Local<Object> versions = Object::New();
