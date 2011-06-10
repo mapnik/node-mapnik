e54fae10c1ac0e0930c182dd56c8a45ac4666358
Dane Springmeyer <dane@dbsgeo.com>
2011-06-10 16:37:49 -0400

    add blank tile server - shows 4000 r/s for pre-encoded image, ~400 r/s for png8, and ~ 200 r/s for full color png - damn zlib inflate

---
+++ examples/simple/blank.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+var mapnik = require('mapnik');
+var http = require('http');
+var path = require('path');
+
+
+// Serve a blank tile. This can indicate the absolute fastest mapnik can return a tile
+// and highlights zlib/inflate bottleneck (compare "png" format to "png8")
+var port = 8000;
+
+var im = new mapnik.Image(256,256)
+
+http.createServer(function(req, res) {
+    res.writeHead(200, {'Content-Type': 'image/png'});
+    res.end(im.encode("png8"));
+}).listen(port);
+
+console.log("server running on port " + port);
