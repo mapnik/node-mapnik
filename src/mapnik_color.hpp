22a6446e8e3c4b90da57386fc519188263d6ee23
Dane Springmeyer <dane@dbsgeo.com>
2011-06-16 01:20:38 -0400

    add mapnik.Color

---
+++ src/mapnik_color.hpp
@@ -0,0 +1,42 @@
+#ifndef __NODE_MAPNIK_COLOR_H__
+#define __NODE_MAPNIK_COLOR_H__
+
+#include <v8.h>
+#include <node.h>
+#include <node_object_wrap.h>
+
+// mapnik
+#include <mapnik/color.hpp>
+
+// boost
+#include <boost/shared_ptr.hpp>
+
+using namespace v8;
+using namespace node;
+
+typedef boost::shared_ptr<mapnik::color> color_ptr;
+
+class Color: public node::ObjectWrap {
+  public:
+    static Persistent<FunctionTemplate> constructor;
+    static void Initialize(Handle<Object> target);
+    static Handle<Value> New(const Arguments &args);
+    static Handle<Value> New(mapnik::color const& color);
+    static Handle<Value> toString(const Arguments &args);
+    static Handle<Value> hex(const Arguments &args);
+
+    static Handle<Value> get_prop(Local<String> property,
+                         const AccessorInfo& info);
+    static void set_prop(Local<String> property,
+                         Local<Value> value,
+                         const AccessorInfo& info);
+
+    Color();
+    inline color_ptr get() { return this_; }
+
+  private:
+    ~Color();
+    color_ptr this_;
+};
+
+#endif
