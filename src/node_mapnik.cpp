782c3fccbb1ce67b05250bc2eded8d301b0895e2
Dane Springmeyer <dane@dbsgeo.com>
2011-08-23 12:20:48 -0700

    bump to 0.5.4

--- src/node_mapnik.cpp
+++ src/node_mapnik.cpp
@@ -93,7 +93,7 @@ extern "C" {
     MemoryDatasource::Initialize(target);
 
     // node-mapnik version
-    target->Set(String::NewSymbol("version"), String::New("0.5.3"));
+    target->Set(String::NewSymbol("version"), String::New("0.5.4"));
 
     // versions of deps
     Local<Object> versions = Object::New();
