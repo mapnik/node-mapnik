8f3373b0d5de6be078881ec119eb7400a9743553
Dane Springmeyer <dane@dbsgeo.com>
2011-11-18 16:16:25 -0800

    bump to 0.6.0 (with first compatibility with node 0.6.0)

--- src/node_mapnik.cpp
+++ src/node_mapnik.cpp
@@ -97,7 +97,7 @@ extern "C" {
     MemoryDatasource::Initialize(target);
 
     // node-mapnik version
-    target->Set(String::NewSymbol("version"), String::New("0.5.9"));
+    target->Set(String::NewSymbol("version"), String::New("0.6.0"));
 
     // versions of deps
     Local<Object> versions = Object::New();
