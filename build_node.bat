a203bdff91966f42f9a6b741c1524ae3c4983e7c
Dane Springmeyer <dane@dbsgeo.com>
2011-11-23 12:41:31 -0800

    comment out lines in windows build script

--- build_node.bat
+++ build_node.bat
@@ -4,6 +4,6 @@ set target=Build
 set config=Release
 python gen_settings.py
 python gyp/gyp build.gyp --depth=. -f msvs -G msvs_version=2010
-;msbuild build.sln /t:%target% /p:Configuration=%config%
+rem msbuild build.sln /t:%target% /p:Configuration=%config%
 msbuild build.sln
-;cp projects/makefiles/out/Default/_mapnik.node lib/_mapnik.node
+rem cp projects/makefiles/out/Default/_mapnik.node lib/_mapnik.node
