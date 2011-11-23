0af395689ab9d6df80ff2fa5dd0cb8e19481f848
Dane Springmeyer <dane@dbsgeo.com>
2011-11-23 13:43:25 -0800

    properly quote the font and plugins paths

--- build_node.bat
+++ build_node.bat
@@ -1,9 +1,12 @@
-set MAPNIK_INPUT_PLUGINS=c:\\mapnik-2.0\\lib\\mapnik\\input
-set MAPNIK_FONTS=c:\\mapnik-2.0\\lib\\mapnik\\fonts
+set MAPNIK_INPUT_PLUGINS="c:\\mapnik-2.0\\lib\\mapnik\\input"
+set MAPNIK_FONTS="c:\\mapnik-2.0\\lib\\mapnik\\fonts"
 set target=Build
 set config=Release
 python gen_settings.py
 python gyp/gyp build.gyp --depth=. -f msvs -G msvs_version=2010
 rem msbuild build.sln /t:%target% /p:Configuration=%config%
 msbuild build.sln
-rem cp projects/makefiles/out/Default/_mapnik.node lib/_mapnik.node
+copy Default\\_mapnik.node lib\\_mapnik.node
+rem test!
+set NODE_PATH=lib
+node -e "console.log(require('mapnik'))"
