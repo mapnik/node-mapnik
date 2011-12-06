e54a09d71808e3e2f2f322da2687d204a0bea305
Dane Springmeyer <dane@dbsgeo.com>
2011-12-05 17:25:32 -0800

    bump to 0.6.2

--- src/node_mapnik.cpp
+++ src/node_mapnik.cpp
@@ -99,7 +99,7 @@ extern "C" {
     Expression::Initialize(target);
 
     // node-mapnik version
-    target->Set(String::NewSymbol("version"), String::New("0.6.1"));
+    target->Set(String::NewSymbol("version"), String::New("0.6.2"));
 
     // versions of deps
     Local<Object> versions = Object::New();
