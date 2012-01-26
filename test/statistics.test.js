be06e471c458e1e069558c1af74aebb219957496
Tom MacWright <tom@macwright.org>
2012-01-26 17:20:38 -0500

    Sub-tests

--- test/statistics.test.js
+++ test/statistics.test.js
@@ -5,27 +5,49 @@ var mapnik = require('mapnik'),
 describe('test memory datasource', function() {
     var options = {};
     var ds = new mapnik.MemoryDatasource(options);
-    ds.parameters().should.be.ok;
-    ds.parameters().should.eql(options);
+
+    it('should be creatable', function() {
+        ds.parameters().should.be.ok;
+        ds.parameters().should.eql(options);
+    });
 
     for (var x = -80; x <= 80; x += 10) {
         for (var y = -80; y <= 80; y += 10) {
-            ds.add({
+            var f = {
                 x: x, y: y,
                 properties: {
                     val: x
                 }
-            });
+            };
+            if (y >= 0) {
+                f.properties.y = y;
+            }
+            ds.add(f);
         }
     }
 
-    ds.features().should.have.lengthOf(289);
-    ds.statistics().should.be.an.instanceof(Object);
-    ds.statistics().val.should.have.property('mean');
-    ds.statistics().val.should.have.property('min');
-    ds.statistics().val.should.have.property('max');
-    ds.statistics().val.mean.should.equal(0);
-    ds.statistics().val.min.should.equal(-80);
-    ds.statistics().val.max.should.equal(80);
-    console.log(ds.statistics());
+    it('should report length and be an object', function() {
+        ds.features().should.have.lengthOf(289);
+        ds.statistics().should.be.an.instanceof(Object);
+    });
+
+    it('should have statistics properties', function() {
+        ds.statistics().val.should.have.property('mean');
+        ds.statistics().val.should.have.property('min');
+        ds.statistics().val.should.have.property('max');
+
+        ds.statistics().y.should.have.property('mean');
+        ds.statistics().y.should.have.property('min');
+        ds.statistics().y.should.have.property('max');
+    });
+
+    it('should record correct statistics', function() {
+        ds.statistics().val.mean.should.equal(0);
+        ds.statistics().val.min.should.equal(-80);
+        ds.statistics().val.max.should.equal(80);
+
+        ds.statistics().y.mean.should.equal(40);
+        ds.statistics().y.min.should.equal(0);
+        ds.statistics().y.max.should.equal(80);
+    });
 });
