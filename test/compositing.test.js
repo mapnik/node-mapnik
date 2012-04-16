501b3bc048bc47f715942da3c711006b55b025be
Dane Springmeyer <dane@dbsgeo.com>
2012-04-16 13:05:04 -0700

    convert to mocha

--- test/compositing.test.js
+++ test/compositing.test.js
@@ -1,26 +1,23 @@
 var mapnik = require('mapnik');
+var assert = require('assert');
 var path = require('path');
 
-exports['test compositing ops'] = function(beforeExit, assert) {
-    rendered = 0;
+describe('mapnik.compositeOp', function() {
     for (var name in mapnik.compositeOp) {
         // http://blog.mixu.net/2011/02/03/javascript-node-js-and-for-loops/
         (function(name) {
             var im1 = mapnik.Image.open('test/support/a.png');
             var im2 = mapnik.Image.open('test/support/b.png');
-            im1.composite(im2,mapnik.compositeOp[name], function(err,im_out) {
-                if (err) throw err;
-                assert.ok(im_out);
-                rendered++;
-                var out = 'test/tmp/' + name + '.png';
-                im_out.save(out);
-                assert.ok(path.existsSync(out));
-            })
+            it('should blend image correctly with op:' + name, function(done) {
+                im1.composite(im2,mapnik.compositeOp[name], function(err,im_out) {
+                    if (err) throw err;
+                    assert.ok(im_out);
+                    var out = 'test/tmp/' + name + '.png';
+                    im_out.save(out);
+                    assert.ok(path.existsSync(out));
+                    done();
+                });
+            });
         })(name)
     }
-
-    beforeExit(function() {
-        assert.equal(rendered, 28);
-    });
-
-};
+});
