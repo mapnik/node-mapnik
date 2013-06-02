c2361e4b9dd01d9c5ea6c6d59a11b138c9482ad6
Dane Springmeyer <dane@dbsgeo.com>
2013-06-02 16:27:45 -0700

    add a test for handling unicode paths from XML - refs mapnik/mapnik#1177

--- test/unicode-loading.test.js
+++ test/unicode-loading.test.js
@@ -1,7 +1,12 @@
 var mapnik = require('mapnik');
 var assert = require('assert');
+var path = require('path');
 var fs = require('fs');
 var existsSync = require('fs').existsSync || require('path').existsSync;
+
+var map_pre = '\n<Map>\n  <Layer name="test">\n    <Datasource>'
+var map_param = '\n      <Parameter name="{{{key}}}">{{{value}}}</Parameter>'
+var map_post = '\n    </Datasource>\n  </Layer>\n</Map>'
 
 describe('Handling unicode paths, filenames, and data', function(){
 
@@ -28,6 +33,20 @@ describe('Handling unicode paths, filenames, and data', function(){
         assert.ok(existsSync(filepath));
         var ds = new mapnik.Datasource({type:'csv',file:filepath});
         assert.ok(ds); 
+    });
+
+    it('open csv file with unicode name in XML', function(){
+        var filepath = './test/data/你好_points.csv';
+        assert.ok(existsSync(filepath));
+        var map_string = map_pre;
+        map_string += map_param.replace('{{{key}}}','type').replace('{{{value}}}','csv');
+        map_string += map_param.replace('{{{key}}}','file').replace('{{{value}}}',filepath);
+        map_string += map_post;
+        var map = new mapnik.Map(256,256);
+        map.fromStringSync(map_string,{base:path.dirname(__dirname)})
+        fs.writeFileSync('/tmp/mapnik-tmp-map-load.xml',map_string,'utf-8')
+        map.loadSync('/tmp/mapnik-tmp-map-load.xml',{base:path.dirname(__dirname)})
+        assert.ok(true);
     });
 
     it('open shape file with unicode name', function(){
