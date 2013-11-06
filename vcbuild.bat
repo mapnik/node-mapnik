5c14cad0859651508b5e96ac6bcda5d1f51e6c81
Dane Springmeyer <dane@dbsgeo.com>
2013-11-05 21:14:51 -0800

    delete old waf and vcbuild files

--- vcbuild.bat
+++ vcbuild.bat
@@ -1,5 +1,5 @@
-set PROJ_LIB=C:\mapnik-v2.2.0\share\proj
-set GDAL_DATA=C:\mapnik-v2.2.0\share\gdal
+set PROJ_LIB=C:\mapnik-v2.3.0\share\proj
+set GDAL_DATA=C:\mapnik-v2.3.0\share\gdal
 rd /q /s build
 del lib\\_mapnik.node
 npm install
