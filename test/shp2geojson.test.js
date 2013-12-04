d4c00977d4c0a1da71bd4f9948643799c44560ba
Dane Springmeyer <dane@dbsgeo.com>
2013-12-04 13:46:39 -0800

    featureset.next() is broken with node v0.6.x so skip this test if using old node

--- test/shp2geojson.test.js
+++ test/shp2geojson.test.js
@@ -4,23 +4,25 @@ var fs = require('fs');
 
 describe('Convert to GeoJSON', function() {
     it('should convert shapefile', function(done) {
-        var ds = new mapnik.Datasource({type:'shape',file:'test/data/world_merc.shp'});
-        var featureset = ds.featureset()
-        var geojson = {
-          "type": "FeatureCollection",
-          "features": [
-          ]
-        }
-        var feat = featureset.next();
-        while (feat) {
-            geojson.features.push(JSON.parse(feat.toJSON()));
-            feat = featureset.next();
-        }
-        var actual = './test/tmp/world_merc.converted.geojson';
-        var expected = './test/data/world_merc.converted.geojson';
-        fs.writeFileSync(actual,JSON.stringify(geojson,null,2));
-        if (mapnik.versions.mapnik_number >= 200300) {
-            assert.equal(fs.readFileSync(actual).length,fs.readFileSync(expected).length)
+        if (process.versions.node.split('.')[1] !== '6') {
+            var ds = new mapnik.Datasource({type:'shape',file:'test/data/world_merc.shp'});
+            var featureset = ds.featureset()
+            var geojson = {
+              "type": "FeatureCollection",
+              "features": [
+              ]
+            }
+            var feat = featureset.next();
+            while (feat) {
+                geojson.features.push(JSON.parse(feat.toJSON()));
+                feat = featureset.next();
+            }
+            var actual = './test/tmp/world_merc.converted.geojson';
+            var expected = './test/data/world_merc.converted.geojson';
+            fs.writeFileSync(actual,JSON.stringify(geojson,null,2));
+            if (mapnik.versions.mapnik_number >= 200300) {
+                assert.equal(fs.readFileSync(actual).length,fs.readFileSync(expected).length)
+            }
         }
         done();
     });
