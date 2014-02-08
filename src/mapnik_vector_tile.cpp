0e532e07e67259ccfb187f472af0a3ee74c92056
Dane Springmeyer <dane@mapbox.com>
2014-02-07 20:13:35 -0800

    shorthand for merc

--- src/mapnik_vector_tile.cpp
+++ src/mapnik_vector_tile.cpp
@@ -270,7 +270,7 @@ Handle<Value> VectorTile::composite(const Arguments& args)
             mapnik::request m_req(target_vt->width(),target_vt->height(),map_extent);
             m_req.set_buffer_size(buffer_size);
             // create map
-            std::string merc_srs("+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0.0 +k=1.0 +units=m +nadgrids=@null +wktext +no_defs +over");
+            std::string merc_srs("+init=epsg:3857");
             mapnik::Map map(target_vt->width(),target_vt->height(),merc_srs);
             map.set_maximum_extent(max_extent);
             // ensure data is in tile object
