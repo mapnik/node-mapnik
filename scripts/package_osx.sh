8da83c27c18436fcec86969b60abf60d934f11b5
Dane Springmeyer <dane@mapbox.com>
2014-05-21 20:58:50 -0700

    use latest mapnik 2.3.x sdk [skip ci]

--- scripts/package_osx.sh
+++ scripts/package_osx.sh
@@ -5,7 +5,7 @@ CURRENT_DIR="$( cd "$( dirname $BASH_SOURCE )" && pwd )"
 cd $CURRENT_DIR/../
 npm install aws-sdk
 rm -rf sdk
-source ./scripts/build_against_sdk.sh
+source ./scripts/build_against_sdk.sh " "
 npm test
 ./node_modules/.bin/node-pre-gyp package testpackage
 npm test
