2eb8357273bd5d42743863f35b77736bb9706955
Dane Springmeyer <dane@mapbox.com>
2014-09-02 22:02:52 -0700

    update Mapnik SDK to 601-g5ae55a0

--- scripts/build_against_sdk.sh
+++ scripts/build_against_sdk.sh
@@ -98,7 +98,7 @@ if [[ "${CXX11:-false}" != false ]]; then
     HASH="1702-g65bd9e6"
 else
     # mapnik 2.3.x / c++11 not enabled
-    HASH="584-g14b6ba9"
+    HASH="601-g5ae55a0"
 fi
 
 if [[ ${platform} == 'linux' ]]; then
