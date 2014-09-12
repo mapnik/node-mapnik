a30888ef047442d525f66c111aa2778c6118359e
Young Hahn <young@mapbox.com>
2014-09-12 08:07:35 -0400

    More detailed assertions for queryMany. Exposes bloatedness of returned features result.

--- test/vector-tile.queryMany.test.js
+++ test/vector-tile.queryMany.test.js
@@ -6,35 +6,6 @@ describe('mapnik.VectorTile queryMany', function() {
 
     it('vtile.queryMany', function(done) {
         mapnik.register_datasource(path.join(mapnik.settings.paths.input_plugins,'ogr.input'));
-        var vtile = new mapnik.VectorTile(0,0,0);
-        var coords = [
-          [0,0],
-          [0.1,0.1],
-          [20,20],
-          [20.1,20.1]
-        ];
-        var geojson = {
-          "type": "FeatureCollection",
-          "features": [
-            {
-              "type": "Feature",
-              "geometry": {
-                "type": "LineString",
-                "coordinates": coords
-              },
-              "properties": {
-                "name": "geojson data"
-              }
-            }
-          ]
-        };
-        vtile.addGeoJSON(JSON.stringify(geojson),"layer-name");
-        var features = vtile.query(0,0,{tolerance:1});
-        assert.equal(features.length,1);
-        assert.equal(features[0].id(),1);
-        assert.ok(features[0].distance < 0.00000001);
-        assert.equal(features[0].layer,'layer-name');
-
         var vtile2 = new mapnik.VectorTile(0,0,0);
         var geojson2 = {
           "type": "FeatureCollection",
@@ -43,25 +14,85 @@ describe('mapnik.VectorTile queryMany', function() {
               "type": "Feature",
               "geometry": {
                 "type": "LineString",
-                "coordinates": coords.reverse()
+                "coordinates": [
+                    [0,0],
+                    [0.1,0.1],
+                    [20,20],
+                    [20.1,20.1]
+                ]
               },
               "properties": {
-                "name": "geojson data"
+                "name": "A"
+              }
+            },
+            {
+              "type": "Feature",
+              "geometry": {
+                "type": "LineString",
+                "coordinates": [
+                    [-40,-40],
+                    [-40.1,-40.1],
+                    [-60,-60],
+                    [-60.1,-60.1]
+                ]
+              },
+              "properties": {
+                "name": "B"
               }
             }
           ]
         };
         vtile2.addGeoJSON(JSON.stringify(geojson2),"layer-name");
-        var manyResults = vtile2.queryMany([[0,0],[0,0]],{tolerance:1,fields:['name'],layer:'layer-name'});
-        assert.equal(manyResults.features.length,2);
+        var manyResults = vtile2.queryMany([[0,0],[0,0],[-40,-40]],{tolerance:1,fields:['name'],layer:'layer-name'});
+        assert.equal(Object.keys(manyResults.hits).length, 3);
+
+        assert.equal(manyResults.hits[0].length, 2);
+        assert.equal(Math.round(manyResults.hits[0][0].distance), 0);
+        assert.equal(manyResults.hits[0][0].feature_id, 0);
+        assert.equal(manyResults.hits[0][1].feature_id, 3);
+        assert.equal(manyResults.features[manyResults.hits[0][0].feature_id].attributes().name, 'A');
+        assert.equal(manyResults.features[manyResults.hits[0][1].feature_id].attributes().name, 'B');
+
+        assert.equal(manyResults.hits[1].length, 2);
+        assert.equal(Math.round(manyResults.hits[1][0].distance), 0);
+        assert.equal(manyResults.hits[1][0].feature_id, 1);
+        assert.equal(manyResults.hits[1][1].feature_id, 4);
+        assert.equal(manyResults.features[manyResults.hits[1][0].feature_id].attributes().name, 'A');
+        assert.equal(manyResults.features[manyResults.hits[1][1].feature_id].attributes().name, 'B');
+
+        assert.equal(manyResults.hits[2].length, 2);
+        assert.equal(Math.round(manyResults.hits[2][0].distance), 6595805);
+        assert.equal(Math.round(manyResults.hits[2][1].distance), 514);
+        assert.equal(manyResults.features[manyResults.hits[2][0].feature_id].attributes().name, 'A');
+        assert.equal(manyResults.features[manyResults.hits[2][1].feature_id].attributes().name, 'B');
+
+
+        assert.equal(manyResults.features.length,6);
+
+        assert.equal(manyResults.features[0].id(),1);
+        assert.equal(manyResults.features[0].layer, 'layer-name');
+        assert.deepEqual(manyResults.features[0].attributes(), { name: 'A' });
+
         assert.equal(manyResults.features[1].id(),1);
-        assert.ok(manyResults.hits['0'][0].distance < 0.00000001);
         assert.equal(manyResults.features[1].layer, 'layer-name');
-        assert.deepEqual(manyResults.features[1].attributes(), { name: 'geojson data' });
-        assert.equal(manyResults.features[1].id(),1);
-        assert.ok(manyResults.hits['1'][0].distance < 0.00000001);
-        assert.equal(manyResults.features[1].layer, 'layer-name');
-        assert.deepEqual(manyResults.features[1].attributes(), { name: 'geojson data' });
+        assert.deepEqual(manyResults.features[1].attributes(), { name: 'A' });
+
+        assert.equal(manyResults.features[2].id(),1);
+        assert.equal(manyResults.features[2].layer, 'layer-name');
+        assert.deepEqual(manyResults.features[2].attributes(), { name: 'A' });
+
+        assert.equal(manyResults.features[3].id(),2);
+        assert.equal(manyResults.features[3].layer, 'layer-name');
+        assert.deepEqual(manyResults.features[3].attributes(), { name: 'B' });
+
+        assert.equal(manyResults.features[4].id(),2);
+        assert.equal(manyResults.features[4].layer, 'layer-name');
+        assert.deepEqual(manyResults.features[4].attributes(), { name: 'B' });
+
+        assert.equal(manyResults.features[5].id(),2);
+        assert.equal(manyResults.features[5].layer, 'layer-name');
+        assert.deepEqual(manyResults.features[5].attributes(), { name: 'B' });
+
         done();
     });
 
