425411d850c0100f41bdc442f25c1db50775b5bb
bergwerkgis <wb@bergwerk-gis.at>
2014-11-18 21:00:03 +0100

    appveyor: binding dir env var not working???

--- scripts/show_module_runtime.ps1
+++ scripts/show_module_runtime.ps1
@@ -3,9 +3,9 @@ Write-Host 'checking runtime of modules ...';
 foreach ($item in Get-ChildItem $args[0] -Recurse -Include *.node){
     $output = dumpbin /DEPENDENTS $item.FullName;
     if ($output -like '*VCRUNTIME140.dll*') {
-        Write-Host "[$item] vcruntime140 found" -ForegroundColor Green
+        Write-Host "vcruntime140 found [$item]"
     } else {
-        Write-Host "[$item] vcruntime140 NOT FOUND" -ForegroundColor Red
+        Write-Host "vcruntime140 NOT FOUND [$item]" -ForegroundColor Red
         Write-Host $output
     }
 }
