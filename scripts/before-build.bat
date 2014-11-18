862f536cc8b6ec84f361d003f4cd9ddd5a2afe08
bergwerkgis <wb@bergwerk-gis.at>
2014-11-18 17:37:06 +0100

    appveyor: copy libs to bindingdir

--- scripts/before-build.bat
+++ scripts/before-build.bat
@@ -1,38 +1,23 @@
 @echo off
 SET EL=0
-:: ============ USAGE
-:: Powershell 3.0 or better needed.
-:: Install from here: http://www.microsoft.com/en-us/download/details.aspx?id=40855
-:: from the node-mapnik clone directory execute this file: scripts\build_against_sdk_00-START-HERE.bat
-:: ============ Powershell Error
-:: if you get "...  cannot be loaded because running scripts is disabled on this system ..."
-:: open an admin prompt and enter: powershell Set-ExecutionPolicy Unrestricted
-SET MAPNIK_SDK=%CD%\mapnik-sdk
-set PATH=%MAPNIK_SDK%\bin;%PATH%
-set PATH=%MAPNIK_SDK%\libs;%PATH%
-set PROJ_LIB=%MAPNIK_SDK%\share\proj
-set GDAL_DATA=%MAPNIK_SDK%\share\gdal
 
-:: upgrade node-gyp to support --msvs_version=2013
-CALL npm install node-gyp
-:: clear out node-gyp header cache
-IF EXIST %USERPROFILE%\.node-gyp rd /s /q %USERPROFILE%\.node-gyp
+SET PATH=%MAPNIK_SDK%\bin;%MAPNIK_SDK%\libs;%PATH%
+SET PROJ_LIB=%MAPNIK_SDK%\share\proj
+SET GDAL_DATA=%MAPNIK_SDK%\share\gdal
+SET ICU_DATA=%MAPNIK_SDK%\share\icu
 
-CALL .\node .\npm\bin\npm-cli.js -v
-CALL set NPM_CACHE=%CD%\npm-cache
-:: npm cache needs to be created beforehand otherwise you'll hit
-:: the error 'uid must be an unsigned int'
-CALL mkdir %NPM_CACHE%
-CALL .\node .\npm\bin\npm-cli.js set cache %NPM_CACHE%
-CALL .\node .\npm\bin\npm-cli.js get cache
-CALL .\node .\npm\bin\npm-cli.js install node-pre-gyp
 CALL node_modules\.bin\node-pre-gyp reveal module --silent > module.txt
 CALL SET /p MODULE=<module.txt
 CALL del module.txt
-CALL python -c "import platform;print(platform.architecture())"
-CALL python -c "import ctypes,os;print('%MODULE%');print os.path.exists('%MODULE%');ctypes.CDLL('%MODULE%')" || true
-CALL .\node .\npm\bin\npm-cli.js install  --no-bin-links --verbose --unsafe-perm
-CALL .\node .\npm\bin\npm-cli.js test || true
+
+call node_modules\.bin\node-pre-gyp reveal module_path --silent > binding_path.txt
+SET /p NODEMAPNIK_BINDING_DIR=<binding_path.txt
+del binding_path.txt
+
+::CALL python -c "import platform;print(platform.architecture())"
+::CALL python -c "import ctypes,os;print('%MODULE%');print os.path.exists('%MODULE%');ctypes.CDLL('%MODULE%')" || true
+::CALL .\node .\npm\bin\npm-cli.js install  --no-bin-links --verbose --unsafe-perm
+:: CALL .\node .\npm\bin\npm-cli.js test || true
 
 
 GOTO DONE
--- scripts/build_against_sdk_02-copy-deps-to-bindingdir.ps1
+++ scripts/build_against_sdk_02-copy-deps-to-bindingdir.ps1
@@ -2,34 +2,25 @@ $msg_prefix='====================== '
 
 Try{
 
-    $deps = Get-ChildItem -Path $env:MAPNIK_LIB_DIR -Filter *.dll | % { $_.FullName }
-
-    ##DELETE mapnik AND share DIRECTORIES, IF THEY EXIST
-    ##POWERSHELL BEHAVES STRANGE IF THEY DO
-    @(
-        $env:N_MAPNIK_LIB_MAPNIK,
-        $env:N_MAPNIK_LIB_SHARE
-    ) |
-    Where-Object { Test-Path $_ } |
-    ForEach-Object { Remove-Item $_ -Recurse -Force -ErrorAction Stop }
+    $deps = Get-ChildItem -Path $env:MAPNIK_SDK\libs -Filter *.dll | % { $_.FullName }
 
 	##COPY DEPENDENCIES TO BINDING DIR
 	Write-Output "$msg_prefix copying dependencies to binding dir:"
 	foreach($dep in $deps){
     	Write-Output $dep
-        Copy-Item $dep $env:N_MAPNIK_BINDING_DIR -ErrorAction Stop
+        Copy-Item $dep $env:NODEMAPNIK_BINDING_DIR -ErrorAction Stop
 	}
 
 	###COPY FONTS AND INPUT PLUGINS TO BINDING DIR
-    $srcDir="$env:MAPNIK_LIB_DIR\mapnik"
-    $destDir=$env:N_MAPNIK_LIB_MAPNIK
+    $srcDir="$env:MAPNIK_SDK\libs\mapnik"
+    $destDir="$env:NODEMAPNIK_BINDING_DIR\mapnik"
 	Write-Output "$msg_prefix copying fonts and input plugins to binding dir:"
 	Write-Output "$srcDir --> $destDir"
     Copy-Item -Path $srcDir -Destination $destDir -Force -Recurse
 
     ##COPY GDAL AND PROJ TO BINDING DIR
-    $srcDir="$env:MAPNIK_DIR\share"
-    $destDir=$env:N_MAPNIK_LIB_SHARE
+    $srcDir="$env:MAPNIK_SDK\share"
+    $destDir="$env:NODEMAPNIK_BINDING_DIR\share"
 	Write-Output "$msg_prefix copying gdal and proj to binding dir:"
 	Write-Output "$srcDir --> $destDir"
     Copy-Item -Path $srcDir -Destination $destDir -Force -Recurse
