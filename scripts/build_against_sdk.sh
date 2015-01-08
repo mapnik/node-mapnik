981337887b94acb383209277000c396ad8e7f0c7
Dane Springmeyer <dane@mapbox.com>
2015-01-07 19:08:50 -0800

    another Mapnik 3.x SDK update

--- scripts/build_against_sdk.sh
+++ scripts/build_against_sdk.sh
@@ -101,12 +101,12 @@ platform=$(echo $UNAME | sed "y/ABCDEFGHIJKLMNOPQRSTUVWXYZ/abcdefghijklmnopqrstu
 # mapnik 3.x / c++11 enabled
 if [[ ${platform} == 'linux' ]]; then
     upgrade_clang
-    TARBALL_NAME="mapnik-${platform}-sdk-v3.0.0-rc1-246-g5361d21"
+    TARBALL_NAME="mapnik-${platform}-sdk-v3.0.0-rc1-249-g481b6b7"
 fi
 
 if [[ $platform == 'darwin' ]]; then
     platform="macosx"
-    TARBALL_NAME="mapnik-${platform}-sdk-v3.0.0-rc1-246-g5361d21-lto"
+    TARBALL_NAME="mapnik-${platform}-sdk-v3.0.0-rc1-249-g481b6b7-lto"
 fi
 
 
