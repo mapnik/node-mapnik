ffaf364cb5d7850a1243515eaed1467af1f18824
Ryan Clark <ryan.clark.j@gmail.com>
2015-02-04 15:22:20 -0700

    add mapnik-shapeindex script. fixes https://github.com/mapnik/node-mapnik/issues/378

---
+++ test/mapnik-shapeindex.test.js
@@ -0,0 +1,52 @@
+"use strict";
+
+var assert = require('assert');
+var path = require('path');
+var fs = require('fs');
+var os = require('os');
+var crypto = require('crypto');
+var cmd = path.resolve(__dirname, '..', 'bin', 'mapnik-shapeindex.js');
+
+var shp = path.join(__dirname, 'data', 'world_merc.shp');
+var shx = path.join(__dirname, 'data', 'world_merc.shx');
+var prj = path.join(__dirname, 'data', 'world_merc.prj');
+var dbf = path.join(__dirname, 'data', 'world_merc.dbf');
+
+var tmpdir = path.join(os.tmpdir(), crypto.randomBytes(8).toString('hex'));
+var tmpshp = path.join(tmpdir, 'world_merc.shp');
+var tmpshx = path.join(tmpdir, 'world_merc.shx');
+var tmpprj = path.join(tmpdir, 'world_merc.prj');
+var tmpdbf = path.join(tmpdir, 'world_merc.dbf');
+
+var exec = require('child_process').exec;
+
+function copy(done) {
+    fs.mkdir(tmpdir, function() {
+        fs.createReadStream(shp).pipe(fs.createWriteStream(tmpshp)).on('close', function() {
+            fs.createReadStream(shx).pipe(fs.createWriteStream(tmpshx)).on('close', function() {
+                fs.createReadStream(prj).pipe(fs.createWriteStream(tmpprj)).on('close', function() {
+                    fs.createReadStream(dbf).pipe(fs.createWriteStream(tmpdbf)).on('close', done);
+                });
+            });
+        });
+    });
+}
+
+describe('bin/mapnik-shapefile.js', function() {
+    before(copy);
+
+    it('should create a spatial index', function(done) {
+        exec([cmd, '--shape_files', tmpshp].join(' '), function(err, stdout, stderr) {
+            assert.ifError(err, 'no error');
+            fs.readdir(tmpdir, function(err, files) {
+
+                files = files.filter(function(filename) {
+                    return filename === 'world_merc.index';
+                });
+
+                assert.equal(files.length, 1, 'made spatial index');
+                done();
+            });
+        });
+    });
+});
