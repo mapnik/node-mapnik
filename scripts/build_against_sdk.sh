e9f1a6be86de266406b8c3c44661f544c00a48df
Dane Springmeyer <dane@mapbox.com>
2015-02-11 12:33:29 -0800

    bump Mapnik 3 SDK again

--- scripts/build_against_sdk.sh
+++ scripts/build_against_sdk.sh
@@ -110,12 +110,12 @@ platform=$(echo $UNAME | sed "y/ABCDEFGHIJKLMNOPQRSTUVWXYZ/abcdefghijklmnopqrstu
 # mapnik 3.x / c++11 enabled
 if [[ ${platform} == 'linux' ]]; then
     upgrade_compiler
-    TARBALL_NAME="mapnik-${platform}-sdk-v3.0.0-rc1-449-g7d2b34d"
+    TARBALL_NAME="mapnik-${platform}-sdk-v3.0.0-rc1-450-g30c6cf6"
 fi
 
 if [[ $platform == 'darwin' ]]; then
     platform="macosx"
-    TARBALL_NAME="mapnik-${platform}-sdk-v3.0.0-rc1-449-g7d2b34d-lto"
+    TARBALL_NAME="mapnik-${platform}-sdk-v3.0.0-rc1-450-g30c6cf6-lto"
 fi
 
 REMOTE_URI="${SDK_URI}/${TARBALL_NAME}.${COMPRESSION}"
