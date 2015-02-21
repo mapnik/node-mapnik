358026b1559deb64c94d544a29fb4b02a5b0683e
Young Hahn <young@mapbox.com>
2015-02-20 19:56:32 -0500

    vt.query: Only include features with distance <= tolerance. Refs #403.

--- src/mapnik_vector_tile.cpp
+++ src/mapnik_vector_tile.cpp
@@ -910,7 +910,7 @@ std::vector<query_result> VectorTile::_query(VectorTile* d, double lon, double l
                             }
                         }
                     }
-                    if (distance >= 0)
+                    if (distance >= 0 && distance <= tolerance)
                     {
                         query_result res;
                         res.distance = distance;
@@ -958,7 +958,7 @@ std::vector<query_result> VectorTile::_query(VectorTile* d, double lon, double l
                             }
                         }
                     }
-                    if (distance >= 0)
+                    if (distance >= 0 && distance <= tolerance)
                     {
                         query_result res;
                         res.distance = distance;
