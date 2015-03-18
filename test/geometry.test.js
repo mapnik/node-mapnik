39a9cdefe182be41d39e64d03f51d33cb12fb348
Dane Springmeyer <dane@mapbox.com>
2015-03-18 01:30:56 -0700

    add more test coverage for mapnik geometry

---
+++ test/geometry.test.js
@@ -0,0 +1,33 @@
+"use strict";
+
+var mapnik = require('../');
+var assert = require('assert');
+var path = require('path');
+
+mapnik.register_datasource(path.join(mapnik.settings.paths.input_plugins,'geojson.input'));
+
+describe('mapnik.Geometry ', function() {
+    it('should throw with invalid usage', function() {
+        // geometry cannot be created directly for now
+        assert.throws(function() { mapnik.Geometry(); });
+    });
+
+    it('should access a geometry from a feature', function() {
+        var feature = new mapnik.Feature(1);
+        var point = {
+         "type": "MultiPoint",
+         "coordinates": [[0,0],[1,1]]
+        };
+        var input = {
+            type: "Feature",
+            properties: {},
+            geometry: point
+        };
+        var f = new mapnik.Feature.fromJSON(JSON.stringify(input));
+        var geom = f.geometry();
+        assert.deepEqual(JSON.parse(geom.toJSONSync()),point);
+        var expected_wkb = new Buffer('0104000000020000000101000000000000000000000000000000000000000101000000000000000000f03f000000000000f03f', 'hex');
+        assert.deepEqual(geom.toWKB(),expected_wkb);
+    });
+
+});
