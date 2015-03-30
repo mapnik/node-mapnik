30a7db2941206e070fe548f51d77a7330b82b34e
Blake Thompson <flippmoke@gmail.com>
2015-03-30 14:15:56 -0500

    Coveraged out some code that will never be reached unless there is bad implementation.

--- src/mapnik_cairo_surface.hpp
+++ src/mapnik_cairo_surface.hpp
@@ -37,7 +37,13 @@ public:
 #if defined(HAVE_CAIRO)
         if (!closure)
         {
+            // Since the closure here is the passing of the std::stringstream "i_stream" of this
+            // class it is unlikely that this could ever be reached unless something was very wrong
+            // and went out of scope, aka it was improperly programmed. Therefore, it is not possible
+            // to reach this point with standard testing.
+            /* LCOV_EXCL_START */
             return CAIRO_STATUS_WRITE_ERROR;
+            /* LCOV_EXCL_END */
         }
         i_stream* fin = reinterpret_cast<i_stream*>(closure);
         *fin << std::string((const char*)data,(size_t)length);
