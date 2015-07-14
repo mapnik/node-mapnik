7e9ff6b81ec0e9ffed475c8cbe00db6f0003d78f
Blake Thompson <flippmoke@gmail.com>
2015-07-14 10:03:55 -0500

    Check that same as rendered image

--- test/image.test.js
+++ test/image.test.js
@@ -1370,4 +1370,24 @@ describe('mapnik.Image ', function() {
             done();
         });
     });
+
+    it('resize should yield the same results as rendered image', function(done) {
+        var im = new mapnik.Image.open('test/data/images/sat_image.png');
+        im.premultiplied = true;
+        im.resize(50,50, {scaling_method:mapnik.imageScaling.sinc}, function(err, result) {
+            if (err) throw err;
+            var map = new mapnik.Map(50,50);
+            map.load('test/data/sat_map.xml', function(err, map) {
+                if (err) throw err;
+                map.zoomAll();
+                var im2 = new mapnik.Image(50,50);
+                map.render(im2, function(err, im2) {
+                    if (err) throw err;
+                    assert.equal(0, result.compare(im2, {threshold:0}));
+                    done();
+                });
+            });
+        });
+
+    });
 });
