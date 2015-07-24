105518371681154cc911a6a9c0a7508be88fa084
ingalls <nicholas.ingalls@gmail.com>
2015-07-24 12:52:06 -0400

    Add block example

--- test/vector-tile.query.test.js
+++ test/vector-tile.query.test.js
@@ -522,9 +522,9 @@ describe('mapnik.VectorTile triangle query', function() {
         vtile.query(170, 80, {}, function(err, data) {
             assert.ifError(err);
             assert.equal(data, false);
-            done()
-        })
-    })
+            done();
+        });
+    });
 });
 
 describe('mapnik.VectorTile query point & line', function() {
@@ -556,24 +556,24 @@ describe('mapnik.VectorTile query point & line', function() {
         //query on point
         var data = vtile.query(0,0, { tolerance: 1000000 });
         assert.equal(data.length, 1);
-        assert.equal(data[0].attributes().name, 'point')
+        assert.equal(data[0].attributes().name, 'point');
 
         //query on line
-        var data = vtile.query(0,10, { tolerance: 1000000 });
+        data = vtile.query(0,10, { tolerance: 1000000 });
         assert.equal(data.length, 1);
-        assert.equal(data[0].attributes().name, 'line')
+        assert.equal(data[0].attributes().name, 'line');
 
         //query between
-        var data = vtile.query(0,5, { tolerance: 1000000 });
+        data = vtile.query(0,5, { tolerance: 1000000 });
         assert.equal(data.length, 2);
-        assert.equal(data[0].attributes().name, 'point')
-        assert.equal(data[1].attributes().name, 'line')
+        assert.equal(data[0].attributes().name, 'point');
+        assert.equal(data[1].attributes().name, 'line');
         done();
     });
 
     it('point on mid line', function(done) {
         // ----o---- <= query on line
-        //mapnik returns two features with identical distances
+        // mapnik returns two features with identical distances (as expected)
 
         var vtile = new mapnik.VectorTile(0,0,0);
         vtile.addGeoJSON(JSON.stringify({
@@ -601,7 +601,7 @@ describe('mapnik.VectorTile query point & line', function() {
         var data = vtile.query(0,0, { tolerance: 1 });
         assert.equal(data.length, 2);
         assert.equal(data[0].distance, data[1].distance);
-        assert.equal(data[0].attributes().name, 'point')
+        assert.equal(data[0].attributes().name, 'point');
         assert.equal(data[1].attributes().name, 'line');
         done();
     });
@@ -632,10 +632,46 @@ describe('mapnik.VectorTile query point & line', function() {
         }]}),'data');
 
         //query on point
-        var data = vtile.query(100,0, { tolerance: 10000 });
+        var data = vtile.query(100,0, { tolerance: 10000 }); //Problematic - this tolerance should be able to be much lower
         assert.equal(data.length, 2);
-        assert.equal(data[0].attributes().name, 'line')
-        assert.equal(data[1].attributes().name, 'point')
+        assert.equal(data[0].attributes().name, 'line');
+        assert.equal(data[1].attributes().name, 'point');
+        done();
+    });
+});
+
+
+describe('mapnik.VectorTile streets', function() {
+    it('block', function(done) {
+        // +--1--+
+        // | o o | pt1 pt2
+        // |     |
+        // 4 o   2 pt3
+        // |     |
+        // | o o | pt4 pt5
+        // +--3--+
+        var vtile = new mapnik.VectorTile(0,0,0);
+        vtile.addGeoJSON(JSON.stringify(require('./data/block.json')),"data");
+
+        var data = vtile.query(-77.0610,38.9066, {tolerance: 10000 });
+        assert.equal(data[0].attributes().name, 'point1');
+
+        //Point 2
+        data = vtile.query(-77.0595,38.9065, {tolerance: 1000 });
+        assert.equal(data[0].attributes().name, 'point2');
+
+        //Point 3
+        data = vtile.query(-77.0610,38.9060, {tolerance: 1000 });
+        assert.equal(data[0].attributes().name, 'point3');
+
+        //Point 4
+        data = vtile.query(-77.0607,38.9053, {tolerance: 1000 });
+        assert.equal(data[0].attributes().name, 'point4');
+
+        //Point 5
+        data = vtile.query(-77.0595,38.9056, {tolerance: 1000 });
+        assert.equal(data[0].attributes().name, 'point5');
+
         done();
     });
 });
