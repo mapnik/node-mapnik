2ac9c027999e37fadcc8c2b04b70aa19dc5fd512
Dane Springmeyer <dane@mapbox.com>
2015-07-30 09:56:07 -0700

    add a lot of code comments to win script

--- scripts/build-appveyor.bat
+++ scripts/build-appveyor.bat
@@ -4,13 +4,14 @@ SET EL=0
 
 ECHO =========== %~f0 ===========
 
-
-::use 64 bit python if platform is 64 bit
+:: use 64 bit python if platform is 64 bit
 IF /I "%PLATFORM%" == "x64" set PATH=C:\Python27-x64;%PATH%
+:: put 7z on path (needed for unpacking mapnik sdk)
 SET PATH=C:\Program Files\7-Zip;%PATH%
-::add local node to path (since we install it below)
-SET PATH=%CD%;%PATH%;
 
+:: TODO - dist/dev/ is intended for dev releases of mapnik
+:: We ideally want to get in the habit of only using Mapnik official releases
+:: which will be uploaded to dist/
 SET MAPNIK_SDK_URL=https://mapnik.s3.amazonaws.com/dist/dev/mapnik-win-sdk-v%MAPNIK_GIT%-%platform%-%msvs_toolset%.0.7z
 ECHO fetching mapnik sdk^: %MAPNIK_SDK_URL%
 IF EXIST mapnik-sdk.7z (ECHO already downloaded) ELSE (powershell Invoke-WebRequest "${env:MAPNIK_SDK_URL}" -OutFile mapnik-sdk.7z)
@@ -19,11 +20,9 @@ ECHO extracting mapnik sdk
 IF EXIST mapnik-sdk (ECHO already extracted) ELSE (7z -y x mapnik-sdk.7z | %windir%\system32\FIND "ing archive")
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
-
-::replace installed node.exe with Mapbox node.exe
-::install node version per visual studio toolset
-IF EXIST node.exe ECHO found local node.exe, deleting ... && DEL /F node.exe
-IF %ERRORLEVEL% NEQ 0 GOTO ERROR
+:: replace installed node.exe with Mapbox node.exe
+:: so that the binaries based on Visual Studio 2015
+:: and built by https://travis-ci.org/mapbox/node-cpp11 are used
 SET ARCHPATH=
 IF /I %platform% == x64 (SET ARCHPATH=x64/)
 SET NODE_URL=https://mapbox.s3.amazonaws.com/node-cpp11/v%nodejs_version%/%ARCHPATH%node.exe
@@ -31,6 +30,11 @@ ECHO fetching node.exe^: %NODE_URL%
 powershell Invoke-WebRequest "${env:NODE_URL}" -OutFile node.exe
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
+:: we replace the existing node.exe with our own
+:: we do this in place so that the npm path logic
+:: is preserved. The alternative might be to put
+:: our custom node.exe on PATH from a custom location
+:: and then pass `--prefix` to npm - but this is untested
 ECHO deleting node.exe programfiles x64
 IF EXIST "%ProgramFiles%\nodejs" ^
     IF EXIST "%ProgramFiles%\nodejs\node.exe" ^
@@ -49,8 +53,11 @@ ECHO copying node.exe to programfiles x86
 IF DEFINED ProgramFiles(x86) IF EXIST "%ProgramFiles(x86)%\nodejs" ECHO copying to "%ProgramFiles(x86)%\nodejs\node.exe" && COPY /Y node.exe "%ProgramFiles(x86)%\nodejs\"
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
-
-ECHO clear out node-gyp header cache
+:: this is not needed on a fresh machine without previous installs
+:: but is important on a machine that already has compiled for a given
+:: node version. So, we have to clear to ensure that the Visual Studio 2015
+:: binaries are used rather than the ones from joyent (which are built with Visual Studio 2013)
+ECHO clear out node-gyp binary cache to ensure vs 2015 binaries are linked
 IF "%msvs_toolset%" == "14" IF EXIST %USERPROFILE%\.node-gyp rd /s /q %USERPROFILE%\.node-gyp
 
 ECHO activating VS command prompt
@@ -62,8 +69,9 @@ where node
 ECHO available npm^:
 where npm
 
-ECHO node -v && node -v
-ECHO node that gets called && node -e "console.log(process.arch,process.execPath)"
+ECHO node -v && CALL node -v
+IF %ERRORLEVEL% NEQ 0 GOTO ERROR
+ECHO node that gets called && CALL node -e "console.log(process.arch,process.execPath)"
 ECHO npm -v && CALL npm -v
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
