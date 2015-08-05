076f0e4f6509ca0b34713efbfa35eab55c434cff
jakepruitt <jake@mapbox.com>
2015-08-04 23:53:53 -0400

    Improve test coverage

--- test/image.svg.test.js
+++ test/image.svg.test.js
@@ -10,11 +10,18 @@ describe('mapnik.Image SVG', function() {
     it('should throw with invalid usage', function() {
         assert.throws(function() { new mapnik.Image.fromSVGSync(); });
         assert.throws(function() { new mapnik.Image.fromSVG(); });
+        assert.throws(function() { new mapnik.Image.fromSVGBytes(); });
         assert.throws(function() {
           new mapnik.Image.fromSVGBytesSync(1);
         }, /must provide a buffer argument/);
         assert.throws(function() {
           new mapnik.Image.fromSVGBytesSync({});
+        }, /first argument is invalid, must be a Buffer/);
+        assert.throws(function() {
+          new mapnik.Image.fromSVGBytes(1, function(err, svg) {});
+        }, /must provide a buffer argument/);
+        assert.throws(function() {
+          new mapnik.Image.fromSVGBytes({}, function(err, svg) {});
         }, /first argument is invalid, must be a Buffer/);
         assert.throws(function() {
           new mapnik.Image.fromSVGBytesSync(new Buffer('asdfasdf'));
@@ -29,14 +36,75 @@ describe('mapnik.Image SVG', function() {
           mapnik.Image.fromSVGSync('./test/data/vector_tile/tile0.expected-svg.svg', 256);
         }, /optional second arg must be an options object/);
         assert.throws(function() {
+          mapnik.Image.fromSVG('./test/data/vector_tile/tile0.expected-svg.svg', 256);
+        }, /last argument must be a callback function/);
+        assert.throws(function() {
+          mapnik.Image.fromSVGBytes(new Buffer('asdfasdf'), 256);
+        }, /last argument must be a callback function/);
+        assert.throws(function() {
+          mapnik.Image.fromSVG('./test/data/vector_tile/tile0.expected-svg.svg', 256, function(err, res) {});
+        }, /optional second arg must be an options object/);
+        assert.throws(function() {
+          mapnik.Image.fromSVGBytes(new Buffer('asdfasdf'), 256, function(err, res) {});
+        }, /optional second arg must be an options object/);
+        assert.throws(function() {
           mapnik.Image.fromSVGSync('./test/data/vector_tile/tile0.expected-svg.svg', { scale: 'foo' });
+        }, /'scale' must be a number/);
+        assert.throws(function() {
+          mapnik.Image.fromSVG('./test/data/vector_tile/tile0.expected-svg.svg', { scale: 'foo' }, function(err, res) {});
+        }, /'scale' must be a number/);
+        assert.throws(function() {
+          mapnik.Image.fromSVGBytes(new Buffer('asdf'), { scale: 'foo' }, function(err, res) {});
         }, /'scale' must be a number/);
         assert.throws(function() {
           mapnik.Image.fromSVGSync('./test/data/vector_tile/tile0.expected-svg.svg', { scale: -1 });
         }, /'scale' must be a positive non zero number/);
         assert.throws(function() {
+          mapnik.Image.fromSVG('./test/data/vector_tile/tile0.expected-svg.svg', { scale: -1 }, function(err, res) {});
+        }, /'scale' must be a positive non zero number/);
+        assert.throws(function() {
+          mapnik.Image.fromSVGBytes(new Buffer('asdf'), { scale: -1 }, function(err, res) {});
+        }, /'scale' must be a positive non zero number/);
+        assert.throws(function() {
           mapnik.Image.fromSVGSync('./test/data/vector_tile/tile0.corrupt-svg.svg');
         }, /image created from svg must have a width and height greater then zero/);
+    });
+
+    it('should err with async file w/o width or height', function(done) {
+      mapnik.Image.fromSVG('./test/data/vector_tile/tile0.corrupt-svg.svg', function(err, svg) {
+        assert.ok(err);
+        assert.ok(err.message.match(/image created from svg must have a width and height greater then zero/));
+        assert.equal(svg, undefined);
+        done();
+      });
+    });
+
+    it('should err with async invalid buffer', function(done) {
+      mapnik.Image.fromSVGBytes(new Buffer('asdfasdf'), function(err, svg) {
+        assert.ok(err);
+        console.log(err);
+        assert.ok(err.message.match(/SVG parse error:\s+Unable to parse 'asdfasdf'/));
+        assert.equal(svg, undefined);
+        done();
+      });
+    });
+
+    it('should err with async non-existent file', function(done) {
+      mapnik.Image.fromSVG('./test/data/SVG_DOES_NOT_EXIST.svg', function(err, svg) {
+        assert.ok(err);
+        assert.ok(err.message.match(/SVG parse error:\s+Unable to open '.\/test\/data\/SVG_DOES_NOT_EXIST.svg'/));
+        assert.equal(svg, undefined);
+        done();
+      });
+    });
+
+    it('should error with async file full of errors', function(done) {
+      mapnik.Image.fromSVG('./test/data/vector_tile/errors.svg', function(err, svg) {
+        assert.ok(err);
+        assert.ok(err.message.match(/SVG parse error:\s+parse_rect: Invalid width\s+Failed to parse double: "FAIL"\s+parse_rect: Invalid height\s+parse_rect: Invalid rx\s+parse_rect: Invalid ry\s+unable to parse invalid svg <path>\s+unable to parse invalid svg <path> with id 'fail-path'\s+unable to parse invalid svg <path> with id 'fail-path'\s+parse_circle: Invalid radius\s+Failed to parse <polygon> 'points'\s+Failed to parse <polyline> 'points'\s+parse_ellipse: Invalid rx\s+parse_ellipse: Invalid ry\s+parse_rect: Invalid height/));
+        assert.equal(svg, undefined);
+        done();
+      });
     });
 
     it('#fromSVGSync load from SVG file', function() {
