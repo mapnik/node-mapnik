8f760d0e073f6ad42ef58856110dc90979a70634
Blake Thompson <flippmoke@gmail.com>
2015-10-20 17:29:03 -0500

    Fixed bad checkin - doh

--- test/vector-tile.test.js
+++ test/vector-tile.test.js
@@ -2297,7 +2297,6 @@ describe('mapnik.VectorTile ', function() {
         });
     });
 
-<<<<<<< Updated upstream
     it('toGeoJSON should not drop geometries outside tile extent', function(done) {
         var vt = new mapnik.VectorTile(10,131,242);
         vt.setData(fs.readFileSync('./test/data/v4-10_131_242.vector.pbf'));
@@ -2305,8 +2304,9 @@ describe('mapnik.VectorTile ', function() {
             assert.equal(layer.features.length, JSON.parse(vt.toGeoJSONSync(layer.name)).features.length);
         })
         done();
-=======
-    it('test that overlapping multipolygon results in two polygons in round trip.', function() {
+    });
+
+    it.skip('test that overlapping multipolygon results in two polygons in round trip.', function() {
         var vtile = new mapnik.VectorTile(0, 0, 0);
         vtile.addGeoJSON(JSON.stringify({
             "type": "FeatureCollection",
@@ -2323,7 +2323,6 @@ describe('mapnik.VectorTile ', function() {
         }), "geojson");
         var expected = [{"name":"geojson","extent":4096,"version":1,"features":[{"id":1,"type":3,"geometry":[[[[2025,2025],[2071,2025],[2071,2071],[2025,2071],[2025,2025]]],[[[2037,2037],[2059,2037],[2059,2059],[2037,2059],[2037,2037]]]],"geometry_type":"MultiPolygon","properties":{}}]}];
         assert.deepEqual(expected, vtile.toJSON({decode_geometry:true}));
->>>>>>> Stashed changes
     });
 
 });
