5cc5e90ccccc660edbb8908241cf2dd24708ee14
bergwerkgis <wb@bergwerk-gis.at>
2016-02-19 13:28:01 +0000

    [skip ci] Windows: local-build.bat, bump to latest mapnik SDK

--- scripts/build-local.bat
+++ scripts/build-local.bat
@@ -10,7 +10,7 @@ ECHO =========== %~f0 ===========
 SET USE_LOCAL_MAPNIK_SDK=0
 
 SET APPVEYOR_REPO_COMMIT_MESSAGE=no commit message provided
-SET MAPNIK_GIT=v3.0.9-rc2-37-g1b6ca8b
+SET MAPNIK_GIT=v3.0.9-246-gd751731
 SET nodejs_version=5.1.0
 SET platform=x64
 SET msvs_toolset=14
