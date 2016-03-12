0775e59bf7090f3694e7d32975bf8f06922c183e
Blake Thompson <flippmoke@gmail.com>
2016-03-11 22:45:31 -0600

    Added a few more tests

--- test/vector-tile.test.js
+++ test/vector-tile.test.js
@@ -3569,4 +3569,63 @@ describe('mapnik.VectorTile ', function() {
         });
     });
     
+    it('pasted test 13 - testing clipper in mapnik vector tile corrects invalid geometry issues', function(done) {
+        var vtile = new mapnik.VectorTile(15,19469,17477);
+        var map = new mapnik.Map(256, 256);
+        map.loadSync('./test/data/vector_tile/pasted/pasted13.xml');
+        map.render(vtile, function(err, vtile) {
+            if (err) throw err;
+            if (hasBoostSimple) {
+                var simplicityReport = vtile.reportGeometrySimplicity();
+                var validityReport = vtile.reportGeometryValidity();
+                assert.equal(simplicityReport.length, 0);
+                assert.equal(validityReport.length, 0); // Dataset not expected to be OGC valid
+            }
+            assert(!vtile.empty());
+            var expected = './test/data/vector_tile/pasted/pasted13.mvt';
+            var actual = './test/data/vector_tile/pasted/pasted13.actual.mvt';
+            if (!existsSync(expected) || process.env.UPDATE) {
+                fs.writeFileSync(expected, vtile.getData());
+            }
+            var expected_data = fs.readFileSync(expected);
+            fs.writeFileSync(actual, vtile.getData());
+            var actual_data = fs.readFileSync(actual);
+            var vt1 = new mapnik.VectorTile(15,19469,17477);
+            vt1.setData(expected_data);
+            var vt2 = new mapnik.VectorTile(15,19469,17477);
+            vt2.setData(actual_data);
+            assert.equal(JSON.stringify(vt1.toJSON()) == JSON.stringify(vt2.toJSON()), true);
+            done();
+        });
+    });
+    
+    it('pasted test 14 - testing clipper in mapnik vector tile corrects invalid geometry issues', function(done) {
+        var vtile = new mapnik.VectorTile(15,19470,17537);
+        var map = new mapnik.Map(256, 256);
+        map.loadSync('./test/data/vector_tile/pasted/pasted14.xml');
+        map.render(vtile, function(err, vtile) {
+            if (err) throw err;
+            if (hasBoostSimple) {
+                var simplicityReport = vtile.reportGeometrySimplicity();
+                var validityReport = vtile.reportGeometryValidity();
+                assert.equal(simplicityReport.length, 0);
+                assert.equal(validityReport.length, 0); // Dataset not expected to be OGC valid
+            }
+            assert(!vtile.empty());
+            var expected = './test/data/vector_tile/pasted/pasted14.mvt';
+            var actual = './test/data/vector_tile/pasted/pasted14.actual.mvt';
+            if (!existsSync(expected) || process.env.UPDATE) {
+                fs.writeFileSync(expected, vtile.getData());
+            }
+            var expected_data = fs.readFileSync(expected);
+            fs.writeFileSync(actual, vtile.getData());
+            var actual_data = fs.readFileSync(actual);
+            var vt1 = new mapnik.VectorTile(15,19470,17537);
+            vt1.setData(expected_data);
+            var vt2 = new mapnik.VectorTile(15,19470,17537);
+            vt2.setData(actual_data);
+            assert.equal(JSON.stringify(vt1.toJSON()) == JSON.stringify(vt2.toJSON()), true);
+            done();
+        });
+    });
 });
