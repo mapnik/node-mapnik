cf65ed18737790042f3a41d331603f7d2fcd5932
mapsam <matthews.sam@gmail.com>
2016-07-18 10:55:25 -0700

    deprecate gh-pages branch in favor of mapnik/documentation

--- tools/fix_cpp_style.sh
+++
@@ -1,6 +0,0 @@
-for file in $(find ./src/ -name '*.*')
-do
-    emacs -batch $file -l `pwd`/tools/cpp.el -f fixup
-done
-
-
--- vcbuild.bat
+++
@@ -1,9 +0,0 @@
-call node -e "console.log(process.version + ' ' + process.arch)"
-set PROJ_LIB=C:\code\mapnik-deps\packages\mapnik-sdk\share\proj
-set GDAL_DATA=C:\code\mapnik-deps\packages\mapnik-sdk\share\gdal
-set PATH=C:\code\mapnik-deps\packages\mapnik-sdk\bin;%PATH%
-set PATH=C:\code\mapnik-deps\packages\mapnik-sdk\libs;%PATH%
-::rd /q /s build
-rd /q /s lib\binding
-::call npm install
-call .\node_modules\.bin\node-pre-gyp build --msvs_version=2013
