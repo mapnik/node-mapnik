b1b7acc717c7f84c2fdc8e6c916f5dc501a3025d
Dane Springmeyer <dane@mapbox.com>
2016-12-04 22:24:47 -0800

    skip large allocation test on appveyor

--- test/image.svg.test.js
+++ test/image.svg.test.js
@@ -91,16 +91,21 @@ describe('mapnik.Image SVG', function() {
 
     });
 
-    it('blocks allocating a very large image', function(done) {
-        // 65535 is the max width/height in mapnik
-        var svgdata = "<svg width='65535' height='65535'><g id='a'><ellipse fill='#FFFFFF' stroke='#000000' stroke-width='4' cx='50' cy='50' rx='25' ry='25'/></g></svg>";
-        var buffer = new Buffer(svgdata);
-        mapnik.Image.fromSVGBytes(buffer, function(err, img) {
-            assert.ok(err);
-            assert.ok(err.message.match(/image created from svg must be 2048 pixels or fewer on each side/));
-            done();
+    if (process.platform === 'win32') {
+        // skip on windows since appveyor does not have enough memory
+        it.skip('blocks allocating a very large image', function(done) {});
+    } else {
+        it('blocks allocating a very large image', function(done) {
+            // 65535 is the max width/height in mapnik
+            var svgdata = "<svg width='65535' height='65535'><g id='a'><ellipse fill='#FFFFFF' stroke='#000000' stroke-width='4' cx='50' cy='50' rx='25' ry='25'/></g></svg>";
+            var buffer = new Buffer(svgdata);
+            mapnik.Image.fromSVGBytes(buffer, function(err, img) {
+                assert.ok(err);
+                assert.ok(err.message.match(/image created from svg must be 2048 pixels or fewer on each side/));
+                done();
+            });
         });
-    });
+    }
 
 
     it('customized the max image size to block', function(done) {
