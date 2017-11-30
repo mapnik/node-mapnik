ccd07da2782401fce9b2ff36b8bb1b5f990d9943
Artem Pavlenko <artem@mapnik.org>
2017-11-30 16:52:43 +0100

    update mason version to 0.17.0

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,7 +3,7 @@
 set -eu
 set -o pipefail
 
-export MASON_RELEASE="${MASON_RELEASE:-v0.16.0}"
+export MASON_RELEASE="${MASON_RELEASE:-v0.17.0}"
 export MASON_LLVM_RELEASE="${MASON_LLVM_RELEASE:-5.0.0}"
 
 PLATFORM=$(uname | tr A-Z a-z)
