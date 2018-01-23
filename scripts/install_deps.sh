9538376ecf20a128e2904654a7335b6449c07715
Dane Springmeyer <dane@mapbox.com>
2018-01-23 09:54:30 -0800

    use boost 1.65.1 - refs #835

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -27,10 +27,10 @@ if [ ! -f ./mason_packages/.link/bin/mapnik-config ]; then
     install cairo 1.14.8
     install webp 0.6.0
     install libgdal 2.1.3
-    install boost 1.63.0
-    install boost_libsystem 1.63.0
-    install boost_libfilesystem 1.63.0
-    install boost_libregex_icu57 1.63.0
+    install boost 1.65.1
+    install boost_libsystem 1.65.1
+    install boost_libfilesystem 1.65.1
+    install boost_libregex_icu57 1.65.1
     install freetype 2.7.1
     install harfbuzz 1.4.2-ft
 
