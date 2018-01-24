8d31c04fe5e1b6b59cc69c71edad7fcf6d1f9a3f
Dane Springmeyer <dane@mapbox.com>
2018-01-24 12:54:31 -0800

    use latest mason version to get mapnik v3.0.17

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,7 +3,7 @@
 set -eu
 set -o pipefail
 
-export MASON_RELEASE="${MASON_RELEASE:-eeba3b5}"
+export MASON_RELEASE="${MASON_RELEASE:-acb5245}"
 export MASON_LLVM_RELEASE="${MASON_LLVM_RELEASE:-5.0.0}"
 
 PLATFORM=$(uname | tr A-Z a-z)
