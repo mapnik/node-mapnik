c89801431cdff035bdb1ef39e71b45adfae01d03
James Seppi <james.seppi@mapbox.com>
2019-08-20 08:33:12 -0500

    Update mapnik version to a0ea7db1a in install_deps.sh (#928)
    * update mapnik version in install_deps.sh
    * update mason version

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -38,5 +38,5 @@ if [ ! -f ./mason_packages/.link/bin/mapnik-config ]; then
     # NOTE: sync this version with the `mapnik_version` in package.json (which is used for windows builds)
     # In the future we could pull from that version automatically if mason were to support knowing the right dep
     # versions to install automatically. Until then there is not much point since the deps are still hardcoded here.
-    install mapnik da69fdf66
+    install mapnik a0ea7db1a
 fi
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,7 +3,7 @@
 set -eu
 set -o pipefail
 
-export MASON_RELEASE="${MASON_RELEASE:-6adb140}"
+export MASON_RELEASE="${MASON_RELEASE:-9d6e1e6}"
 export MASON_LLVM_RELEASE="${MASON_LLVM_RELEASE:-5.0.1}"
 
 PLATFORM=$(uname | tr A-Z a-z)
