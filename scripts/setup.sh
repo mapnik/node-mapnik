89c991f28f81d7a02c51b932b381c9e45bd23b2f
Artem Pavlenko <artem@mapnik.org>
2020-10-16 14:42:53 +0100

    revert mapnik upgrade, publish dev package 4.5.3-imagetest.2 [publish binary]

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,7 +3,7 @@
 set -eu
 set -o pipefail
 
-export MASON_RELEASE="${MASON_RELEASE:-cce4ebf4}"
+export MASON_RELEASE="${MASON_RELEASE:-70dd16d}"
 export MASON_LLVM_RELEASE="${MASON_LLVM_RELEASE:-10.0.0}"
 
 PLATFORM=$(uname | tr A-Z a-z)
