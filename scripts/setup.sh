c62f3ad17e78095fd02ff2db04aa567d0dbdda7c
Artem Pavlenko <artem@mapnik.org>
2021-04-17 15:15:41 +0100

    Update mason version to 3bf6d4f [republish binary]

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,7 +3,7 @@
 set -eu
 set -o pipefail
 
-export MASON_RELEASE="${MASON_RELEASE:-03a3ea40}"
+export MASON_RELEASE="${MASON_RELEASE:-3bf6d4f}"
 export MASON_LLVM_RELEASE="${MASON_LLVM_RELEASE:-10.0.0}"
 
 PLATFORM=$(uname | tr A-Z a-z)
