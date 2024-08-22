de0f95bffba04a4fec5174a81ccbfd90e59085e0
Artem Pavlenko <artem@mapnik.org>
2024-08-22 15:44:44 +0100

    cleanup

--- scripts/postgis/setup_and_run.sh
+++
@@ -1,4 +0,0 @@
-./scripts/postgis/setup.sh
-./scripts/postgis/initialize.sh
-./scripts/postgis/run.sh
-source mason-postgis-config.env
--- scripts/postgis/stop.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -eu
-set -o pipefail
-
-source mason-postgis-config.env && ./mason_packages/.link/bin/pg_ctl -w stop
